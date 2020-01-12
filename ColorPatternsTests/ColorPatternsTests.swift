de3fc83bd5fbc95a2862a6bf4bc68d568b18dd99
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-01-12 23:24:15 +0100

    Add simple preformance test

--- ColorPatternsTests/ColorPatternsTests.swift
+++ ColorPatternsTests/ColorPatternsTests.swift
@@ -8,7 +8,11 @@
 
 import XCTest
 
+@testable import ColorPatterns
+
 class ColorPatternsTests: XCTestCase {
+
+    var colors = Colors()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -24,10 +28,8 @@ class ColorPatternsTests: XCTestCase {
     }
 
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            _ = colors.shuffle()
         }
     }
-
 }
