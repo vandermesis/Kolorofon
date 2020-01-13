61df52268a837a2055a32b90795894eb48dc611e
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-01-13 15:00:51 +0100

    Change rootViewController to GameController
    Changes:
    	Modify Appdelegate to display viewcontroller from xib
    	Connect Game view to controller
    	Initial setup of GameController
    	Setup GameCreator
    	Initial setup of GameViewModel
    	Remove Main.storyboard

--- ColorPatterns/Scenes/Game/GameController.swift
+++ ColorPatterns/Scenes/Game/GameController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class GameController: UIViewController {
+final class GameController: UIViewController {
+
+    @IBOutlet private var colorBars: [UIButton]!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var scoreLabel: UILabel!
+
+    private let viewModel: GameViewModel
+
+    init(viewModel: GameViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override var prefersStatusBarHidden: Bool {
+      return true
     }
-    */
-
 }
--- ColorPatterns/Scenes/Game/GameCreator.swift
+++ ColorPatterns/Scenes/Game/GameCreator.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+struct GameCreator {
+
+    func getController() -> GameController {
+
+        let viewModel = GameViewModel()
+        let controller = GameController(viewModel: viewModel)
+
+        return controller
+    }
+}
