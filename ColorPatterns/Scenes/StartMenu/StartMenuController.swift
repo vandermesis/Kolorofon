96a30a06f734813aa949b981c6177532a5bc762f
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-02-23 18:00:43 +0100

    Add empty info scene

--- ColorPatterns/Scenes/StartMenu/StartMenuController.swift
+++ ColorPatterns/Scenes/StartMenu/StartMenuController.swift
@@ -38,18 +38,26 @@ final class StartMenuController: UIViewController {
         viewModel.checkGameCenterStatus()
     }
 
+    @IBAction private func infoButtonPressed(_ sender: UIButton) {
+        let infoController = InfoCreator().getController()
+        infoController.modalPresentationStyle = .fullScreen
+        present(infoController, animated: true, completion: nil)
+    }
+
     @IBAction private func startButtonPressed(_ sender: UIButton) {
         let gameController = GameCreator().getController(difficulty: viewModel.difficulty)
         gameController.modalPresentationStyle = .fullScreen
         present(gameController, animated: true, completion: nil)
 
     }
+
     @IBAction private func difficultySegmentChanged(_ sender: UISegmentedControl) {
         let selectedSegment = sender.selectedSegmentIndex
         guard let segmentTitle = sender.titleForSegment(at: selectedSegment) else { return }
         guard let difficulty = Level(rawValue: segmentTitle.lowercased()) else { return }
         viewModel.chooseDifficulty(level: difficulty)
     }
+
     @IBAction private func bestScoresButtonPressed(_ sender: UIButton) {
         presentGameCenterController()
     }
