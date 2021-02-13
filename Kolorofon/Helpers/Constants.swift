914197facb73cf5a2ef35f15ae81977ccc2346f6
Marek Skrzelowski <vandermesis@mac-pro-marek.local>
2021-02-13 02:28:41 +0100

    Connect new leaderboards

--- Kolorofon/Helpers/Constants.swift
+++ Kolorofon/Helpers/Constants.swift
@@ -11,29 +11,24 @@ import Foundation
 struct Constants {
 
     struct Sounds {
-
         static let note1 = "note1"
         static let note2 = "note2"
         static let note3 = "note3"
         static let note4 = "note4"
         static let note5 = "note5"
         static let note6 = "note6"
-        
         static let audioFormat = "caf"
     }
 
     struct UserDefaultsKeys {
-
         static let sound = "Sound"
     }
 
     struct GameTimer {
-
         static let gameTime = 90
     }
 
     struct SettingsBundleKeys {
-
         static let buildVersionKey = "build_preference"
         static let appVersionKey = "version_preference"
         static let cfBundleVersionStringShort = "CFBundleShortVersionString"
@@ -41,9 +36,8 @@ struct Constants {
     }
 
     struct GameCenter {
-
-        static let easyLeaderBoardID = "com.bestscores.colourpatterns"
-        static let mediumLeaderBoardID = "com.mediumbestscores.colourpatterns"
-        static let hardLeaderBoardID = "com.hardbestscores.colourpatterns"
+        static let easyLeaderBoardID = "com.easy.kolorofon"
+        static let mediumLeaderBoardID = "com.medium.kolorofon"
+        static let hardLeaderBoardID = "com.hard.kolorofon"
     }
 }
